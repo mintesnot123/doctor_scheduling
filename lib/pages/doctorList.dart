0b264185d9013c37a98f48125441bfdd59441572
FlutLab.io <>
2022-01-12 12:25:39 +0000

    Autosave

--- lib/pages/doctorList.dart
+++ lib/pages/doctorList.dart
@@ -157,6 +157,68 @@ class _DoctorsListState extends State<DoctorsList> {
                 SizedBox(
                   height: 30,
                 ),
+                TextFormField(
+                  controller: _textController,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[300],
+                    hintText: 'Search Doctor',
+                    hintStyle: GoogleFonts.lato(
+                      color: Colors.black26,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w800,
+                    ),
+                    prefixIcon: Icon(
+                      FlutterIcons.search1_ant,
+                      size: 20,
+                    ),
+                    prefixStyle: TextStyle(
+                      color: Colors.grey[300],
+                    ),
+                    suffixIcon: _textController.text.length != 0
+                        ? TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _textController.clear();
+                                _length = search.length;
+                              });
+                            },
+                            child: Icon(
+                              Icons.close_rounded,
+                              size: 20,
+                            ),
+                          )
+                        : SizedBox(),
+                  ),
+                  // onFieldSubmitted: (String _searchKey) {
+                  //   setState(
+                  //     () {
+                  //       print('>>>' + _searchKey);
+                  //       search = _searchKey;
+                  //     },
+                  //   );
+                  // },
+                  onChanged: (String _searchKey) {
+                    setState(
+                      () {
+                        print('>>>' + _searchKey);
+                        search = _searchKey;
+                        _length = search.length;
+                      },
+                    );
+                  },
+                  style: GoogleFonts.lato(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textInputAction: TextInputAction.search,
+                  autofocus: false,
+                ),
                 (_length == 0
                     ? Center(
                         child: Container(
