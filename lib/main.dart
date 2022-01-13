25e1837c2885156812bb280abda1cb7c6982d5e9
FlutLab.io <>
2022-01-13 20:37:10 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 import 'package:yismaw/pages/splash.dart';
 import 'package:yismaw/pages/login.dart';
@@ -27,6 +28,21 @@ class MyApp extends StatelessWidget {
     user = _auth.currentUser;
   }
 
+  Color _primaryColor = HexColor('#DC54FE');
+  Color _accentColor = HexColor('#8A02AE');
+
+  // Design color
+  // Color _primaryColor= HexColor('#FFC867');
+  // Color _accentColor= HexColor('#FF3CBD');
+
+  // Our Logo Color
+  // Color _primaryColor= HexColor('#D44CF6');
+  // Color _accentColor= HexColor('#5E18C8');
+
+  // Our Logo Blue Color
+  //Color _primaryColor= HexColor('#651BD2');
+  //Color _accentColor= HexColor('#320181');
+
   @override
   Widget build(BuildContext context) {
     _getUser();
@@ -38,9 +54,13 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Doctors scheduling',
         theme: ThemeData(
-          textTheme: TextTheme(
+          /* textTheme: TextTheme(
             bodyText1: TextStyle(fontFamily: 'Poppins'),
-          ),
+          ), */
+          primaryColor: _primaryColor,
+          accentColor: _accentColor,
+          scaffoldBackgroundColor: Colors.grey.shade100,
+          primarySwatch: Colors.grey,
         ),
         initialRoute: '/',
         routes: {
--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -10,7 +10,7 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
-  AnimationController mainController;
+  /* AnimationController mainController;
   Animation mainAnimation;
   @override
   void initState() {
@@ -23,6 +23,21 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
     mainController.forward();
     mainController.addListener(() {
       setState(() {});
+    });
+  } */
+  bool _isVisible = false;
+
+  _SplashPageState() {
+    new Timer(const Duration(milliseconds: 2000), () {
+      setState(() {
+        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) => LoginPage()), (route) => false);
+      });
+    });
+
+    new Timer(Duration(milliseconds: 10), () {
+      setState(() {
+        _isVisible = true; // Now it is showing fade effect and navigating to Login page
+      });
     });
   }
 
@@ -87,7 +102,7 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
       ),
     );
 
-    return Scaffold(
+    /* return Scaffold(
       backgroundColor: mainAnimation.value,
       body: SafeArea(
         child: Container(
@@ -119,6 +134,49 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
                 footer,
               ],
             ),
+          ),
+        ),
+      ),
+    ); */
+    return Container(
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
+          colors: [
+            Theme.of(context).accentColor,
+            Theme.of(context).primaryColor
+          ],
+          begin: const FractionalOffset(0, 0),
+          end: const FractionalOffset(1.0, 0.0),
+          stops: [
+            0.0,
+            1.0
+          ],
+          tileMode: TileMode.clamp,
+        ),
+      ),
+      child: AnimatedOpacity(
+        opacity: _isVisible ? 1.0 : 0,
+        duration: Duration(milliseconds: 1200),
+        child: Center(
+          child: Container(
+            height: 140.0,
+            width: 140.0,
+            child: Center(
+              child: ClipOval(
+                child: Icon(
+                  Icons.android_outlined,
+                  size: 128,
+                ), //put your logo here
+              ),
+            ),
+            decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.white, boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                blurRadius: 2.0,
+                offset: Offset(5.0, 3.0),
+                spreadRadius: 2.0,
+              )
+            ]),
           ),
         ),
       ),
