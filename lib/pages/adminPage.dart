8021a6f911fbf31523991d922ddc2bf9e76a9123
FlutLab.io <>
2022-01-13 08:35:50 +0000

    Autosave

--- lib/pages/adminPage.dart
+++ lib/pages/adminPage.dart
@@ -47,36 +47,25 @@ class Home extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.done) {
           Map<String, dynamic> data = snapshot.data.data() as Map<String, dynamic>;
           return checkRole(data);
-          //return Text("Full Name: ${data['full_name']} ${data['last_name']}");
         }
-
-        return Scaffold(body: LinearProgressIndicator());
+        return Scaffold(body: Center(child: LinearProgressIndicator()));
       },
-      /* builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else if (snapshot.hasData) {
-            return checkRole(snapshot.data);
-          }
-          return LinearProgressIndicator();
-        }, */
     );
   }
 
   Widget checkRole(Map<String, dynamic> snapshot) {
     if (snapshot['role'] == 'ADMIN') {
       return MainPage();
+    } else if (snapshot['role'] == 'DOCTOR') {
+      return DoctorPage();
+    } else if (snapshot['role'] == 'ASSOCIATE') {
+      return AssociatePage();
     } else {
-      return userPage(snapshot);
+      return Scaffold(
+          body: Center(
+        child: Text("Something went wrong"),
+      ));
     }
-  }
-
-  Scaffold adminPage(Map<String, dynamic> snapshot) {
-    return Scaffold(body: Center(child: Text('${snapshot['role']} ${snapshot['name']}')));
-  }
-
-  Scaffold userPage(Map<String, dynamic> snapshot) {
-    return Scaffold(body: Center(child: Text('${snapshot['name']}')));
   }
 }
 
@@ -86,6 +75,314 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  int _selectedIndex = 0;
+  List<Widget> _pages = [
+    HomePage(),
+    DoctorsList(),
+    HomePage(),
+    //DoctorsList(),
+    //Center(child: Text('New Appointment')),
+    //MyAppointments(),
+    UserProfile(),
+  ];
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  User user;
+
+  Future<void> _getUser() async {
+    user = _auth.currentUser;
+  }
+
+  _navigate(Widget screen) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) => screen));
+  }
+
+  String shortcut = "no action set";
+
+  @override
+  void initState() {
+    super.initState();
+    _getUser();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        key: _scaffoldKey,
+        body: _pages[_selectedIndex],
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home),
+                selected: _selectedIndex == 0,
+                onTap: () {
+                  _onItemTapped(0);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text("Doctors"),
+                leading: Icon(Icons.home),
+                selected: _selectedIndex == 1,
+                onTap: () {
+                  /* _openPage(0); */
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home),
+                selected: _selectedIndex == 2,
+                onTap: () {
+                  /* _openPage(0); */
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 20,
+                color: Colors.black.withOpacity(.2),
+              ),
+            ],
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8),
+              child: GNav(
+                curve: Curves.easeOutExpo,
+                rippleColor: Colors.grey[300],
+                hoverColor: Colors.grey[100],
+                haptic: true,
+                tabBorderRadius: 20,
+                gap: 5,
+                activeColor: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                duration: Duration(milliseconds: 400),
+                tabBackgroundColor: Colors.blue.withOpacity(0.7),
+                textStyle: GoogleFonts.lato(
+                  color: Colors.white,
+                ),
+                tabs: [
+                  GButton(
+                    iconSize: _selectedIndex != 0 ? 28 : 25,
+                    icon: _selectedIndex == 0 ? FlutterIcons.home_fou : FlutterIcons.home_variant_outline_mco,
+                    text: 'Home',
+                  ),
+                  GButton(
+                    icon: FlutterIcons.person_mdi,
+                    text: 'Doctors',
+                  ),
+                  GButton(
+                    iconSize: _selectedIndex != 2 ? 20 : 15,
+                    icon: /* _selectedIndex == 2 ? */ FlutterIcons.clinic_medical_faw5s /* : Typicons.calendar_outline */,
+                    text: 'Schedule',
+                  ),
+                  GButton(
+                    iconSize: 29,
+                    icon: _selectedIndex == 3 ? Typicons.user : Typicons.user_outline,
+                    text: 'Profile',
+                  ),
+                ],
+                selectedIndex: _selectedIndex,
+                onTabChange: _onItemTapped,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DoctorPage extends StatefulWidget {
+  @override
+  _DoctorPageState createState() => _DoctorPageState();
+}
+
+class _DoctorPageState extends State<DoctorPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  int _selectedIndex = 0;
+  List<Widget> _pages = [
+    HomePage(),
+    DoctorsList(),
+    HomePage(),
+    //DoctorsList(),
+    //Center(child: Text('New Appointment')),
+    //MyAppointments(),
+    UserProfile(),
+  ];
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  User user;
+
+  Future<void> _getUser() async {
+    user = _auth.currentUser;
+  }
+
+  _navigate(Widget screen) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (_) => screen));
+  }
+
+  String shortcut = "no action set";
+
+  @override
+  void initState() {
+    super.initState();
+    _getUser();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        key: _scaffoldKey,
+        body: _pages[_selectedIndex],
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home),
+                selected: _selectedIndex == 0,
+                onTap: () {
+                  _onItemTapped(0);
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text("Doctors"),
+                leading: Icon(Icons.home),
+                selected: _selectedIndex == 1,
+                onTap: () {
+                  /* _openPage(0); */
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home),
+                selected: _selectedIndex == 2,
+                onTap: () {
+                  /* _openPage(0); */
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 20,
+                color: Colors.black.withOpacity(.2),
+              ),
+            ],
+          ),
+          child: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8),
+              child: GNav(
+                curve: Curves.easeOutExpo,
+                rippleColor: Colors.grey[300],
+                hoverColor: Colors.grey[100],
+                haptic: true,
+                tabBorderRadius: 20,
+                gap: 5,
+                activeColor: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                duration: Duration(milliseconds: 400),
+                tabBackgroundColor: Colors.blue.withOpacity(0.7),
+                textStyle: GoogleFonts.lato(
+                  color: Colors.white,
+                ),
+                tabs: [
+                  GButton(
+                    iconSize: _selectedIndex != 0 ? 28 : 25,
+                    icon: _selectedIndex == 0 ? FlutterIcons.home_fou : FlutterIcons.home_variant_outline_mco,
+                    text: 'Home',
+                  ),
+                  GButton(
+                    icon: FlutterIcons.person_mdi,
+                    text: 'Doctors',
+                  ),
+                  GButton(
+                    iconSize: _selectedIndex != 2 ? 20 : 15,
+                    icon: /* _selectedIndex == 2 ? */ FlutterIcons.clinic_medical_faw5s /* : Typicons.calendar_outline */,
+                    text: 'Schedule',
+                  ),
+                  GButton(
+                    iconSize: 29,
+                    icon: _selectedIndex == 3 ? Typicons.user : Typicons.user_outline,
+                    text: 'Profile',
+                  ),
+                ],
+                selectedIndex: _selectedIndex,
+                onTabChange: _onItemTapped,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AssociatePage extends StatefulWidget {
+  @override
+  _AssociatePageState createState() => _AssociatePageState();
+}
+
+class _AssociatePageState extends State<AssociatePage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   int _selectedIndex = 0;
   List<Widget> _pages = [
