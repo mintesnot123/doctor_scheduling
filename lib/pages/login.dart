e6fad4fd4740f3ebd68db832953f4d78be6a387f
FlutLab.io <>
2022-01-13 20:59:09 +0000

    Autosave

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -222,7 +222,6 @@ class _LoginPageState extends State<LoginPage> {
   } */
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
@@ -233,94 +232,96 @@ class _LoginPageState extends State<LoginPage> {
               child: HeaderWidget(_headerHeight, true, Icons.login_rounded), //let's create a common header widget
             ),
             SafeArea(
-              child: Container( 
-                padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),// This will be the login form
-                child: Column(
-                  children: [
-                    Text(
-                      'Hello',
-                      style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Signin into your account',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    SizedBox(height: 30.0),
-                    Form(
-                      key: _formKey,
-                        child: Column(
-                          children: [
-                            Container(
-                              child: TextField(
-                                decoration: ThemeHelper().textInputDecoration('User Name', 'Enter your user name'),
-                              ),
-                              decoration: ThemeHelper().inputBoxDecorationShaddow(),
-                            ),
-                            SizedBox(height: 30.0),
-                            Container(
-                              child: TextField(
-                                obscureText: true,
-                                decoration: ThemeHelper().textInputDecoration('Password', 'Enter your password'),
-                              ),
-                              decoration: ThemeHelper().inputBoxDecorationShaddow(),
-                            ),
-                            SizedBox(height: 15.0),
-                            Container(
-                              margin: EdgeInsets.fromLTRB(10,0,10,20),
-                              alignment: Alignment.topRight,
-                              child: GestureDetector(
-                                onTap: () {
-                                  /* Navigator.push( context, MaterialPageRoute( builder: (context) => ForgotPasswordPage()), ); */
-                                },
-                                child: Text( "Forgot your password?", style: TextStyle( color: Colors.grey, ),
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10), // This will be the login form
+                  child: Column(
+                    children: [
+                      Text(
+                        'Hello',
+                        style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Signin into your account',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      SizedBox(height: 30.0),
+                      Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              Container(
+                                child: TextField(
+                                  decoration: ThemeHelper().textInputDecoration('User Name', 'Enter your user name'),
                                 ),
+                                decoration: ThemeHelper().inputBoxDecorationShaddow(),
                               ),
-                            ),
-                            Container(
-                              decoration: ThemeHelper().buttonBoxDecoration(context),
-                              child: ElevatedButton(
-                                style: ThemeHelper().buttonStyle(),
-                                child: Padding(
-                                  padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
-                                  child: Text('Sign In'.toUpperCase(), style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),),
+                              SizedBox(height: 30.0),
+                              Container(
+                                child: TextField(
+                                  obscureText: true,
+                                  decoration: ThemeHelper().textInputDecoration('Password', 'Enter your password'),
                                 ),
-                                onPressed: (){
-                                  //After successful login we will redirect to profile page. Let's create profile page now
-                                  /* Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProfilePage())); */
-                                },
+                                decoration: ThemeHelper().inputBoxDecorationShaddow(),
                               ),
-                            ),
-                            Container(
-                              margin: EdgeInsets.fromLTRB(10,20,10,20),
-                              //child: Text('Don\'t have an account? Create'),
-                              child: Text.rich(
-                                TextSpan(
-                                  children: [
-                                    TextSpan(text: "Don\'t have an account? "),
-                                    TextSpan(
-                                      text: 'Create',
-                                      recognizer: TapGestureRecognizer()
-                                        ..onTap = (){
-                                          /* Navigator.push(context, MaterialPageRoute(builder: (context) => RegistrationPage())); */
-                                        },
-                                      style: TextStyle(fontWeight: FontWeight.bold, color: Theme.of(context).accentColor),
+                              SizedBox(height: 15.0),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 0, 10, 20),
+                                alignment: Alignment.topRight,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    /* Navigator.push( context, MaterialPageRoute( builder: (context) => ForgotPasswordPage()), ); */
+                                  },
+                                  child: Text(
+                                    "Forgot your password?",
+                                    style: TextStyle(
+                                      color: Colors.grey,
                                     ),
-                                  ]
-                                )
+                                  ),
+                                ),
                               ),
-                            ),
-                          ],
-                        )
-                    ),
-                  ],
-                )
-              ),
+                              Container(
+                                decoration: ThemeHelper().buttonBoxDecoration(context),
+                                child: ElevatedButton(
+                                  style: ThemeHelper().buttonStyle(),
+                                  child: Padding(
+                                    padding: EdgeInsets.fromLTRB(40, 10, 40, 10),
+                                    child: Text(
+                                      'Sign In'.toUpperCase(),
+                                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    //After successful login we will redirect to profile page. Let's create profile page now
+                                    /* Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ProfilePage())); */
+                                  },
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                                //child: Text('Don\'t have an account? Create'),
+                                child: Text.rich(TextSpan(children: [
+                                  TextSpan(text: "Don\'t have an account? "),
+                                  TextSpan(
+                                    text: 'Create',
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        /* Navigator.push(context, MaterialPageRoute(builder: (context) => RegistrationPage())); */
+                                      },
+                                    style: TextStyle(fontWeight: FontWeight.bold, color: Theme.of(context).accentColor),
+                                  ),
+                                ])),
+                              ),
+                            ],
+                          )),
+                    ],
+                  )),
             ),
           ],
         ),
       ),
     );
+  }
 
   void processError(final FirebaseAuthException error) {
     if (error.code == "user-not-found") {
