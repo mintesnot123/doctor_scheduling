99232cf1bf18eac9ac3a458534164022c151abbc
FlutLab.io <>
2022-01-15 20:09:18 +0000

    Autosave

--- lib/pages/main/doctorsList.dart
+++ lib/pages/main/doctorsList.dart
@@ -278,7 +278,7 @@ class _DoctorsListPageState extends State<DoctorsListPage> {
                                                               child: FittedBox(
                                                                 child: Icon(
                                                                   Icons.check,
-                                                                  color: Colors.blue[900],
+                                                                  color: Colors.red[900],
                                                                   size: 400,
                                                                 ),
                                                               ),
