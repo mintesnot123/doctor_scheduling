db43c6d3f92b650013a3372577d797900e4657fe
FlutLab.io <>
2022-01-15 18:51:25 +0000

    Autosave

--- lib/pages/main/dashboard2.dart
+++ lib/pages/main/dashboard2.dart
@@ -74,208 +74,199 @@ class _DashboardPageState extends State<DashboardPage> {
                 ),
                 SafeArea(
                   child: Container(
-                      /* padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                      margin: EdgeInsets.fromLTRB(20, 10, 20, 10), */ // This will be the login form
-                      child: Column(
-                    children: [
-                      /* Text(
-                            'Hello',
-                            style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            'Signin into your account',
-                            style: TextStyle(color: Colors.grey),
-                          ), */
-                      SizedBox(
-                        height: 30,
-                      ),
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.only(left: 20, bottom: 10),
-                        child: Text(
-                          "Hello " + (user.displayName ?? "User"),
-                          style: GoogleFonts.lato(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                          ),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 20,
                         ),
-                      ),
-                      Container(
-                        alignment: Alignment.centerLeft,
-                        padding: EdgeInsets.only(left: 20, bottom: 25),
-                        child: Text(
-                          "Let's Find Your\nDoctor",
-                          style: GoogleFonts.lato(
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.fromLTRB(20, 0, 20, 25),
-                        child: TextFormField(
-                          textInputAction: TextInputAction.search,
-                          controller: _doctorName,
-                          decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                              borderSide: BorderSide.none,
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.only(left: 20, bottom: 10),
+                          child: Text(
+                            "Hello " + (user.displayName ?? "User"),
+                            style: GoogleFonts.lato(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
                             ),
-                            filled: true,
-                            fillColor: Colors.grey[200],
-                            hintText: 'Search doctor',
-                            hintStyle: GoogleFonts.lato(
-                              color: Colors.black26,
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.only(left: 20, bottom: 25),
+                          child: Text(
+                            "Let's Find Your\nDoctor",
+                            style: GoogleFonts.lato(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(20, 0, 20, 25),
+                          child: TextFormField(
+                            textInputAction: TextInputAction.search,
+                            controller: _doctorName,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                                borderSide: BorderSide.none,
+                              ),
+                              filled: true,
+                              fillColor: Colors.grey[200],
+                              hintText: 'Search doctor',
+                              hintStyle: GoogleFonts.lato(
+                                color: Colors.black26,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w800,
+                              ),
+                              suffixIcon: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.blue[900].withOpacity(0.9),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: IconButton(
+                                  iconSize: 20,
+                                  splashRadius: 20,
+                                  color: Colors.white,
+                                  icon: Icon(FlutterIcons.search1_ant),
+                                  onPressed: () {
+                                    _doctorName.text.length == 0
+                                        ? Container()
+                                        : Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) => SearchList(
+                                                searchKey: _doctorName.text,
+                                              ),
+                                            ),
+                                          );
+                                  },
+                                ),
+                              ),
+                            ),
+                            style: GoogleFonts.lato(
                               fontSize: 18,
                               fontWeight: FontWeight.w800,
                             ),
-                            suffixIcon: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.blue[900].withOpacity(0.9),
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              child: IconButton(
-                                iconSize: 20,
-                                splashRadius: 20,
-                                color: Colors.white,
-                                icon: Icon(FlutterIcons.search1_ant),
-                                onPressed: () {
-                                  _doctorName.text.length == 0
+                            onFieldSubmitted: (String value) {
+                              setState(
+                                () {
+                                  value.length == 0
                                       ? Container()
                                       : Navigator.push(
                                           context,
                                           MaterialPageRoute(
                                             builder: (context) => SearchList(
-                                              searchKey: _doctorName.text,
+                                              searchKey: value,
                                             ),
                                           ),
                                         );
                                 },
-                              ),
-                            ),
+                              );
+                            },
                           ),
-                          style: GoogleFonts.lato(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w800,
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 23, bottom: 10),
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "We care for you",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.lato(color: Colors.blue[800], fontWeight: FontWeight.bold, fontSize: 18),
                           ),
-                          onFieldSubmitted: (String value) {
-                            setState(
-                              () {
-                                value.length == 0
-                                    ? Container()
-                                    : Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => SearchList(
-                                            searchKey: value,
-                                          ),
-                                        ),
-                                      );
-                              },
-                            );
-                          },
                         ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(left: 23, bottom: 10),
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "We care for you",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.lato(color: Colors.blue[800], fontWeight: FontWeight.bold, fontSize: 18),
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Carouselslider(),
                         ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        child: Carouselslider(),
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(left: 20),
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "Specialists",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.lato(color: Colors.blue[800], fontWeight: FontWeight.bold, fontSize: 18),
+                        Container(
+                          padding: EdgeInsets.only(left: 20),
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Specialists",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.lato(color: Colors.blue[800], fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
                         ),
-                      ),
-                      Container(
-                        height: 150,
-                        padding: EdgeInsets.only(top: 14),
-                        child: ListView.builder(
-                          physics: ClampingScrollPhysics(),
-                          scrollDirection: Axis.horizontal,
-                          padding: EdgeInsets.symmetric(horizontal: 20.0),
-                          itemCount: cards.length,
-                          itemBuilder: (context, index) {
-                            //print("images path: ${cards[index].cardImage.toString()}");
-                            return Container(
-                              margin: EdgeInsets.only(right: 14),
-                              height: 150,
-                              width: 140,
-                              decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Color(cards[index].cardBackground), boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey[400],
-                                  blurRadius: 4.0,
-                                  spreadRadius: 0.0,
-                                  offset: Offset(3, 3),
-                                ),
-                              ]
-                                  // image: DecorationImage(
-                                  //   image: AssetImage(cards[index].cardImage),
-                                  //   fit: BoxFit.fill,
-                                  // ),
+                        Container(
+                          height: 150,
+                          padding: EdgeInsets.only(top: 14),
+                          child: ListView.builder(
+                            physics: ClampingScrollPhysics(),
+                            scrollDirection: Axis.horizontal,
+                            padding: EdgeInsets.symmetric(horizontal: 20.0),
+                            itemCount: cards.length,
+                            itemBuilder: (context, index) {
+                              //print("images path: ${cards[index].cardImage.toString()}");
+                              return Container(
+                                margin: EdgeInsets.only(right: 14),
+                                height: 150,
+                                width: 140,
+                                decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: Color(cards[index].cardBackground), boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey[400],
+                                    blurRadius: 4.0,
+                                    spreadRadius: 0.0,
+                                    offset: Offset(3, 3),
                                   ),
-                              // ignore: deprecated_member_use
-                              child: FlatButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => ExploreList(
-                                              type: cards[index].doctor,
-                                            )),
-                                  );
-                                },
-                                shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20)),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    SizedBox(
-                                      height: 16,
+                                ]
+                                    // image: DecorationImage(
+                                    //   image: AssetImage(cards[index].cardImage),
+                                    //   fit: BoxFit.fill,
+                                    // ),
                                     ),
-                                    Container(
-                                      child: CircleAvatar(
-                                          backgroundColor: Colors.white,
-                                          radius: 29,
-                                          child: Icon(
-                                            cards[index].cardIcon,
-                                            size: 26,
-                                            color: Color(cards[index].cardBackground),
-                                          )),
-                                    ),
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    Container(
-                                      alignment: Alignment.bottomCenter,
-                                      child: Text(
-                                        cards[index].doctor,
-                                        style: GoogleFonts.lato(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600),
+                                // ignore: deprecated_member_use
+                                child: FlatButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => ExploreList(
+                                                type: cards[index].doctor,
+                                              )),
+                                    );
+                                  },
+                                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20)),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      SizedBox(
+                                        height: 16,
                                       ),
-                                    ),
-                                  ],
+                                      Container(
+                                        child: CircleAvatar(
+                                            backgroundColor: Colors.white,
+                                            radius: 29,
+                                            child: Icon(
+                                              cards[index].cardIcon,
+                                              size: 26,
+                                              color: Color(cards[index].cardBackground),
+                                            )),
+                                      ),
+                                      SizedBox(
+                                        height: 10,
+                                      ),
+                                      Container(
+                                        alignment: Alignment.bottomCenter,
+                                        child: Text(
+                                          cards[index].doctor,
+                                          style: GoogleFonts.lato(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                            );
-                          },
+                              );
+                            },
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 30,
-                      ),
-                    ],
-                  )),
+                        SizedBox(
+                          height: 30,
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
