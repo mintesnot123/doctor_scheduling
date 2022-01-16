cab9d8b816e85f50b85d430c3b62899eb11a2caa
FlutLab.io <>
2022-01-16 19:46:28 +0000

    Autosave

--- lib/pages/main/doctorAppointments.dart
+++ lib/pages/main/doctorAppointments.dart
@@ -2,6 +2,7 @@ import 'package:yismaw/widgets/header_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:yismaw/pages/main/appointmentsList.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DoctorAppointmentsScreen extends StatefulWidget {
   final String doctor;
