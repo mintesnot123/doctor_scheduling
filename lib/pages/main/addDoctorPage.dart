d945ff65f5deb2a9fc1b8f270ed361f5c8f7884a
FlutLab.io <>
2022-01-16 07:58:51 +0000

    Autosave

--- lib/pages/main/addDoctorPage.dart
+++ lib/pages/main/addDoctorPage.dart
@@ -4,6 +4,7 @@ import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
