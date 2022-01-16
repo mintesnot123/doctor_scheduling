e6713de44c1cf379e2ecbd0dbc0d8a3312ed6785
FlutLab.io <>
2022-01-16 12:04:29 +0000

    Autosave

--- lib/pages/main/doctorDetail.dart
+++ lib/pages/main/doctorDetail.dart
@@ -48,12 +48,112 @@ class _DoctorDetailState extends State<DoctorDetail> {
               height: 100,
               child: HeaderWidget(100, false, Icons.house_rounded),
             ),
-            SizedBox(
-              height: 30,
-            ),
-            SafeArea(
-              child: Text(widget.doctor),
-            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(width: 5, color: Colors.white),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 20,
+                          offset: const Offset(5, 5),
+                        ),
+                      ],
+                    ),
+                    child: Icon(
+                      Icons.person,
+                      size: 80,
+                      color: Colors.grey.shade300,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Mr. Donald Trump',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Former President',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.only(left: 8.0, bottom: 4.0),
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            "User Information",
+                            style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Card(
+                          child: Container(
+                            alignment: Alignment.topLeft,
+                            padding: EdgeInsets.all(15),
+                            child: Column(
+                              children: <Widget>[
+                                Column(
+                                  children: <Widget>[
+                                    ...ListTile.divideTiles(
+                                      color: Colors.grey,
+                                      tiles: [
+                                        ListTile(
+                                          contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                                          leading: Icon(Icons.my_location),
+                                          title: Text("Location"),
+                                          subtitle: Text("USA"),
+                                        ),
+                                        ListTile(
+                                          leading: Icon(Icons.email),
+                                          title: Text("Email"),
+                                          subtitle: Text("donaldtrump@gmail.com"),
+                                        ),
+                                        ListTile(
+                                          leading: Icon(Icons.phone),
+                                          title: Text("Phone"),
+                                          subtitle: Text("99--99876-56"),
+                                        ),
+                                        ListTile(
+                                          leading: Icon(Icons.person),
+                                          title: Text("About Me"),
+                                          subtitle: Text("This is a about me link and you can khow about me in this section."),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
