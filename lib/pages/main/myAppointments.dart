5688efcb42b40cd620e128b12a3e9d5b5b07d8a2
FlutLab.io <>
2022-01-17 11:19:51 +0000

    Autosave

--- lib/pages/main/myAppointments.dart
+++ lib/pages/main/myAppointments.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:yismaw/pages/main/appointmentsList.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class MyAppointmentsScreen extends StatefulWidget {
   const MyAppointmentsScreen({
