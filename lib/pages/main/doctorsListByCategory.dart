873301acf5e87e426e54f401284a3d5b3341c44f
FlutLab.io <>
2022-02-01 12:06:40 +0000

    Autosave

--- lib/pages/main/doctorsListByCategory.dart
+++ lib/pages/main/doctorsListByCategory.dart
@@ -101,9 +101,7 @@ class _DoctorsListByCategoryState extends State<DoctorsListByCategory> {
                           ),
                         )
                       : Scrollbar(
-                          child: SizedBox(
-                            height: 30,
-                          ), /* Column(
+                          child: Column(
                             children: [
                               Container(
                                 padding: EdgeInsets.only(left: 20),
@@ -116,138 +114,111 @@ class _DoctorsListByCategoryState extends State<DoctorsListByCategory> {
                               ),
                               SizedBox(
                                 height: 30,
-                              ), */
-                          /* ListView.builder(
-                            scrollDirection: Axis.vertical,
-                            physics: ClampingScrollPhysics(),
-                            shrinkWrap: true,
-                            itemCount: snapshot.data.size,
-                            itemBuilder: (context, index) {
-                              DocumentSnapshot doctor = snapshot.data.docs[index];
-                              return Padding(
-                                padding: const EdgeInsets.only(top: 0.0),
-                                child: Card(
-                                  color: Colors.blue[50],
-                                  elevation: 0,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: Container(
-                                    padding: EdgeInsets.only(left: 10, right: 10, top: 0),
-                                    width: MediaQuery.of(context).size.width,
-                                    height: MediaQuery.of(context).size.height / 9,
-                                    child: TextButton(
-                                      onPressed: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(
-                                            builder: (context) => DoctorDetail(
-                                              doctor: doctor.id,
-                                              role: widget.role,
-                                            ),
-                                          ),
-                                        );
-                                      },
-                                      child: Row(
-                                        crossAxisAlignment: CrossAxisAlignment.center,
-                                        //mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                        children: [
-                                          (doctor['approved'] == "APPROVED")
-                                              ? Flexible(
-                                                  flex: 1,
-                                                  child: Stack(
-                                                    children: <Widget>[
-                                                      CircleAvatar(
-                                                        backgroundImage: NetworkImage('https://cdn.pixabay.com/photo/2017/11/02/14/26/model-2911329_960_720.jpg' /* doctor['image'] */),
-                                                        radius: 30,
-                                                      ),
-                                                      Positioned(
-                                                        bottom: 0,
-                                                        right: 0,
-                                                        child: Container(
-                                                          width: 15.0,
-                                                          height: 15.0,
-                                                          decoration: BoxDecoration(
-                                                            color: Colors.white,
-                                                            shape: BoxShape.circle,
-                                                          ),
-                                                          child: FittedBox(
-                                                            child: Icon(
-                                                              Icons.check,
-                                                              color: Colors.red[900],
-                                                              size: 24,
-                                                            ),
-                                                          ),
-                                                        ),
-                                                      )
-                                                    ],
-                                                  ),
-                                                )
-                                              : CircleAvatar(
-                                                  backgroundImage: NetworkImage('https://cdn.pixabay.com/photo/2017/11/02/14/26/model-2911329_960_720.jpg' /* doctor['image'] */),
-                                                  radius: 30,
+                              ),
+                              ListView.builder(
+                                scrollDirection: Axis.vertical,
+                                physics: ClampingScrollPhysics(),
+                                shrinkWrap: true,
+                                itemCount: snapshot.data.size,
+                                itemBuilder: (context, index) {
+                                  DocumentSnapshot doctor = snapshot.data.docs[index];
+                                  return Padding(
+                                    padding: const EdgeInsets.only(top: 0.0),
+                                    child: Card(
+                                      color: Colors.blue[50],
+                                      elevation: 0,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      child: Container(
+                                        padding: EdgeInsets.only(left: 10, right: 10, top: 0),
+                                        width: MediaQuery.of(context).size.width,
+                                        height: MediaQuery.of(context).size.height / 9,
+                                        child: TextButton(
+                                          onPressed: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (context) => DoctorDetail(
+                                                  doctor: doctor.id,
+                                                  role: widget.role,
                                                 ),
-                                          SizedBox(
-                                            width: 20,
-                                          ),
-                                          Column(
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            mainAxisAlignment: MainAxisAlignment.center,
+                                              ),
+                                            );
+                                          },
+                                          child: Row(
+                                            crossAxisAlignment: CrossAxisAlignment.center,
+                                            //mainAxisAlignment: MainAxisAlignment.spaceAround,
                                             children: [
-                                              Text(
-                                                'Dr. ${doctor['name'] ?? 'User'}'.length > 18 ? 'Dr. ${doctor['name'] ?? 'User'}'.substring(0, 18) + "..." : 'Dr. ${doctor['name'] ?? 'User'}',
-                                                style: GoogleFonts.lato(
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 17,
-                                                  color: Colors.black87,
-                                                ),
+                                              (doctor['approved'] == "APPROVED")
+                                                  ? Flexible(
+                                                      flex: 1,
+                                                      child: Stack(
+                                                        children: <Widget>[
+                                                          CircleAvatar(
+                                                            backgroundImage: NetworkImage('https://cdn.pixabay.com/photo/2017/11/02/14/26/model-2911329_960_720.jpg' /* doctor['image'] */),
+                                                            radius: 30,
+                                                          ),
+                                                          Positioned(
+                                                            bottom: 0,
+                                                            right: 0,
+                                                            child: Container(
+                                                              width: 15.0,
+                                                              height: 15.0,
+                                                              decoration: BoxDecoration(
+                                                                color: Colors.white,
+                                                                shape: BoxShape.circle,
+                                                              ),
+                                                              child: FittedBox(
+                                                                child: Icon(
+                                                                  Icons.check,
+                                                                  color: Colors.red[900],
+                                                                  size: 24,
+                                                                ),
+                                                              ),
+                                                            ),
+                                                          )
+                                                        ],
+                                                      ),
+                                                    )
+                                                  : CircleAvatar(
+                                                      backgroundImage: NetworkImage('https://cdn.pixabay.com/photo/2017/11/02/14/26/model-2911329_960_720.jpg' /* doctor['image'] */),
+                                                      radius: 30,
+                                                    ),
+                                              SizedBox(
+                                                width: 20,
                                               ),
-                                              Text(
-                                                '${doctor['type'] ?? "Specialist Doctor"}'.length > 30 ? '${doctor['type'] ?? "Specialist Doctor"}'.substring(0, 27) + "..." : '${doctor['type'] ?? "Specialist Doctor"}',
-                                                style: GoogleFonts.lato(fontSize: 16, color: Colors.black54),
-                                              ),
-                                            ],
-                                          ),
-                                          SizedBox(
-                                            width: 10,
-                                          ),
-                                          /* Expanded(
-                                            child: Container(
-                                              alignment: Alignment.centerRight,
-                                              child: Row(
-                                                crossAxisAlignment: CrossAxisAlignment.end,
-                                                mainAxisAlignment: MainAxisAlignment.end,
+                                              Column(
+                                                crossAxisAlignment: CrossAxisAlignment.start,
+                                                mainAxisAlignment: MainAxisAlignment.center,
                                                 children: [
-                                                  Icon(
-                                                    Typicons.star_full_outline,
-                                                    size: 20,
-                                                    color: Colors.indigo[400],
-                                                  ),
-                                                  SizedBox(
-                                                    width: 3,
-                                                  ),
                                                   Text(
-                                                    doctor['rating'].toString(),
+                                                    'Dr. ${doctor['name'] ?? 'User'}'.length > 18 ? 'Dr. ${doctor['name'] ?? 'User'}'.substring(0, 18) + "..." : 'Dr. ${doctor['name'] ?? 'User'}',
                                                     style: GoogleFonts.lato(
                                                       fontWeight: FontWeight.bold,
-                                                      fontSize: 15,
-                                                      color: Colors.indigo,
+                                                      fontSize: 17,
+                                                      color: Colors.black87,
                                                     ),
+                                                  ),
+                                                  Text(
+                                                    '${doctor['type'] ?? "Specialist Doctor"}'.length > 30 ? '${doctor['type'] ?? "Specialist Doctor"}'.substring(0, 27) + "..." : '${doctor['type'] ?? "Specialist Doctor"}',
+                                                    style: GoogleFonts.lato(fontSize: 16, color: Colors.black54),
                                                   ),
                                                 ],
                                               ),
-                                            ),
-                                          ), */
-                                        ],
+                                              SizedBox(
+                                                width: 10,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ), */
-                          /* ],
-                          ), */
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
                         );
                 },
               ),
