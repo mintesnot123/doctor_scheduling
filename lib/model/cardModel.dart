1f6a42fd028cc8b795f7b513aa60c37a06b64836
FlutLab.io <>
2022-02-01 12:34:58 +0000

    Autosave

--- lib/model/cardModel.dart
+++ lib/model/cardModel.dart
@@ -12,8 +12,9 @@ class CardModel {
 
 List<CardModel> cards = [
   new CardModel("CONSULTANT PHYSICIAN", 0xFFec407a, FlutterIcons.heart_ant),
-  new CardModel("Dentist", 0xFF5c6bc0, FlutterIcons.tooth_mco),
-  new CardModel("Eye Special", 0xFFfbc02d, TablerIcons.eye),
-  new CardModel("Orthopaedic", 0xFF1565C0, Icons.wheelchair_pickup_sharp),
-  new CardModel("Paediatrician", 0xFF2E7D32, FlutterIcons.baby_faw5s),
+  new CardModel("ORTHOPEDICIAN", 0xFF5c6bc0, FlutterIcons.tooth_mco),
+  new CardModel("NEUROLOGIST", 0xFFfbc02d, TablerIcons.eye),
+  new CardModel("GENERAL SURGEON", 0xFF1565C0, Icons.wheelchair_pickup_sharp),
+  new CardModel("URO SURGEON", 0xFF2E7D32, FlutterIcons.baby_faw5s),
+  new CardModel("CHEST SPECIALIST", 0xFFfbc02d, TablerIcons.eye),
 ];
