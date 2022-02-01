99f80c810a92983f103f15ffb27f5540ebfb7657
FlutLab.io <>
2022-02-01 11:35:48 +0000

    Autosave

--- lib/model/cardModel.dart
+++ lib/model/cardModel.dart
@@ -11,7 +11,7 @@ class CardModel {
 }
 
 List<CardModel> cards = [
-  new CardModel("Cardiologist", 0xFFec407a, FlutterIcons.heart_ant),
+  new CardModel("CONSULTANT PHYSICIAN", 0xFFec407a, FlutterIcons.heart_ant),
   new CardModel("Dentist", 0xFF5c6bc0, FlutterIcons.tooth_mco),
   new CardModel("Eye Special", 0xFFfbc02d, TablerIcons.eye),
   new CardModel("Orthopaedic", 0xFF1565C0, Icons.wheelchair_pickup_sharp),
