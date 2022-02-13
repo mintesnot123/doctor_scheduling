392604523462689d6c7e30297b328997ca096217
FlutLab.io <>
2022-02-13 16:40:32 +0000

    Autosave

--- lib/pages/doctorList.dart
+++ lib/pages/doctorList.dart
@@ -32,6 +32,21 @@ class _DoctorsListState extends State<DoctorsList> {
     super.dispose();
   }
 
+  Future getData() {
+    FirebaseFirestore.instance.collection('doctors').get().then((QuerySnapshot querySnapshot) {
+      querySnapshot.docs.forEach((doc) {
+        print(doc["first_name"]);
+      });
+    });
+    //FirebaseFirestore.instance.collection('doctors').orderBy('name').snapshots()
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
