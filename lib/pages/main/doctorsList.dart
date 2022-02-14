fb591bbc9625c019dc67cfad7776f7708c6a45a7
FlutLab.io <>
2022-02-14 05:30:28 +0000

    Autosave

--- lib/pages/main/doctorsList.dart
+++ lib/pages/main/doctorsList.dart
@@ -261,6 +261,166 @@ class _DoctorsListPageState extends State<DoctorsListPage> {
                         SizedBox(
                           height: 30,
                         ),
+                        Container((doctors.length > 0)
+                            ? Scrollbar(
+                                child: ListView.builder(
+                                  scrollDirection: Axis.vertical,
+                                  physics: ClampingScrollPhysics(),
+                                  shrinkWrap: true,
+                                  itemCount: doctors.length,
+                                  itemBuilder: (context, index) {
+                                    DocumentSnapshot doctor = doctors[index];
+                                    return Padding(
+                                      padding: const EdgeInsets.only(top: 0.0),
+                                      child: Card(
+                                        color: Colors.blue[50],
+                                        elevation: 0,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(10),
+                                        ),
+                                        child: Container(
+                                          padding: EdgeInsets.only(left: 10, right: 10, top: 0),
+                                          width: MediaQuery.of(context).size.width,
+                                          height: MediaQuery.of(context).size.height / 9,
+                                          child: TextButton(
+                                            onPressed: () {
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                  builder: (context) => DoctorDetail(doctor: doctor.id, role: widget.role),
+                                                ),
+                                              );
+                                            },
+                                            child: Row(
+                                              crossAxisAlignment: CrossAxisAlignment.center,
+                                              //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                              children: [
+                                                (doctor['approved'] == "APPROVED")
+                                                    ? Flexible(
+                                                        flex: 1,
+                                                        child: Stack(
+                                                          children: <Widget>[
+                                                            CircleAvatar(
+                                                              backgroundImage: AssetImage('assets/images/doctor_profile.jpg'), //NetworkImage('https://cdn.pixabay.com/photo/2017/11/02/14/26/model-2911329_960_720.jpg' /* doctor['image'] */),
+                                                              radius: 30,
+                                                            ),
+                                                            Positioned(
+                                                              bottom: 0,
+                                                              right: 0,
+                                                              child: Container(
+                                                                width: 15.0,
+                                                                height: 15.0,
+                                                                decoration: BoxDecoration(
+                                                                  color: Colors.white,
+                                                                  shape: BoxShape.circle,
+                                                                ),
+                                                                child: FittedBox(
+                                                                  child: Icon(
+                                                                    Icons.check,
+                                                                    color: Colors.red[900],
+                                                                    size: 24,
+                                                                  ),
+                                                                ),
+                                                              ),
+                                                            )
+                                                          ],
+                                                        ),
+                                                      )
+                                                    : CircleAvatar(
+                                                        backgroundImage: AssetImage('assets/images/doctor_profile.jpg'), //NetworkImage('https://cdn.pixabay.com/photo/2017/11/02/14/26/model-2911329_960_720.jpg' /* doctor['image'] */),
+                                                        radius: 30,
+                                                      ),
+                                                SizedBox(
+                                                  width: 20,
+                                                ),
+                                                Column(
+                                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                                  mainAxisAlignment: MainAxisAlignment.center,
+                                                  children: [
+                                                    Text(
+                                                      'Dr. ${doctor['name'] ?? 'User'}'.length > 18 ? 'Dr. ${doctor['name'] ?? 'User'}'.substring(0, 18) + "..." : 'Dr. ${doctor['name'] ?? 'User'}',
+                                                      style: GoogleFonts.lato(
+                                                        fontWeight: FontWeight.bold,
+                                                        fontSize: 17,
+                                                        color: Colors.black87,
+                                                      ),
+                                                    ),
+                                                    Text(
+                                                      '${doctor['type'] ?? "Specialist Doctor"}'.length > 30 ? '${doctor['type'] ?? "Specialist Doctor"}'.substring(0, 27) + "..." : '${doctor['type'] ?? "Specialist Doctor"}',
+                                                      style: GoogleFonts.lato(fontSize: 16, color: Colors.black54),
+                                                    ),
+                                                  ],
+                                                ),
+                                                SizedBox(
+                                                  width: 10,
+                                                ),
+                                                /* Expanded(
+                                                      child: Container(
+                                                        alignment: Alignment.centerRight,
+                                                        child: Row(
+                                                          crossAxisAlignment: CrossAxisAlignment.end,
+                                                          mainAxisAlignment: MainAxisAlignment.end,
+                                                          children: [
+                                                            Icon(
+                                                              Typicons.star_full_outline,
+                                                              size: 20,
+                                                              color: Colors.indigo[400],
+                                                            ),
+                                                            SizedBox(
+                                                              width: 3,
+                                                            ),
+                                                            Text(
+                                                              doctor['rating'].toString(),
+                                                              style: GoogleFonts.lato(
+                                                                fontWeight: FontWeight.bold,
+                                                                fontSize: 15,
+                                                                color: Colors.indigo,
+                                                              ),
+                                                            ),
+                                                          ],
+                                                        ),
+                                                      ),
+                                                    ), */
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              )
+                            : (loadError != ''
+                                ? Center(
+                                    child: CircularProgressIndicator(),
+                                  )
+                                : (doctors.length == 0
+                                    ? Center(
+                                        child: Container(
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.center,
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: [
+                                              Text(
+                                                'No Doctor found!',
+                                                style: GoogleFonts.lato(
+                                                  color: Colors.blue[800],
+                                                  fontSize: 25,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              Image(
+                                                image: AssetImage('assets/images/error-404.jpg'),
+                                                height: 250,
+                                                width: 250,
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      )
+                                    : Center(
+                                        child: CircularProgressIndicator(),
+                                      )))),
                         StreamBuilder(
                           stream: FirebaseFirestore.instance.collection('users').where("role", isEqualTo: "DOCTOR").orderBy('name').snapshots(),
                           builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
